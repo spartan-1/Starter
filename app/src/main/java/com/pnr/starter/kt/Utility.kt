1db38d20fec78381af803f93da584253622b665c
301171210 <Pnr@1234>
2019-05-19 10:33:05 +0530

    Initial commit- base code

---
+++ app/src/main/java/com/pnr/starter/kt/Utility.kt
@@ -0,0 +1,4 @@
+package com.pnr.starter.kt
+
+class Utility {
+}
---
+++ app/src/main/java/com/pnr/starter/kt/ui/activities/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.pnr.starter.kt.ui.activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.pnr.starter.kt.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
